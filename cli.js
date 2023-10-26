bc8de8944e1b0cdf95e6fae2b68f0a571bc398d6
Robin Millette <robin@millette.info>
2023-10-26 17:58:47 -0400

    Rewrite.

--- cli.js
+++ cli.js
@@ -1,9 +1,43 @@
 #!/usr/bin/env node
 
-// self
-const run = require(".")
+// node
+import { readFile, writeFile } from "node:fs/promises"
+import { basename } from "node:path"
+
+// npm
+import fg from "fast-glob"
 
 const fixFrom = "http://localhost:4873/"
 const fixTo = "https://registry.npmjs.org/"
+const fixToY = "https://registry.yarnpkg.com/"
+const lockFiles = ["yarn.lock", "package-lock.json", "pnpm-lock.yaml"]
+const options = { cwd: process.cwd(), absolute: true }
 
-run(fixFrom, fixTo)
+function rep(cnt2, fix) {
+  const cnt = cnt2.replaceAll(fixFrom, fix)
+  return {
+    cnt,
+    fixes: (cnt.length - cnt2.length) / (fix.length - fixFrom.length),
+  }
+}
+
+async function run() {
+  const fixed = []
+  for await (const entry of fg.stream(lockFiles, options)) {
+    const lockfile = basename(entry)
+    let cnt2 = await readFile(entry, "utf-8")
+    const { cnt, fixes } = rep(cnt2, lockfile === lockFiles[0] ? fixToY : fixTo)
+    if (fixes) {
+      await writeFile(entry, cnt)
+      fixed.push({ lockfile, fixes })
+    }
+  }
+  return fixed
+}
+
+try {
+  const res = await run()
+  console.log(res)
+} catch (e) {
+  console.error(e)
+}
